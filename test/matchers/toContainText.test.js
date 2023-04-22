38a073f36d9e40c743b2a033cf7a200435c1dd30
clong desktop <chip_long@yahoo.com>
2023-04-21 22:56:56 -0600

    added custom toContainText matcher

---
+++ test/matchers/toContainText.test.js
@@ -0,0 +1,51 @@
+import { toContainText } from "./toContainText";
+
+const stripTerminalColor = (text) => text.replace(/\x1B\[\d+m/g, "");
+
+describe("toContainText matcher", () => {
+  const textToFind = "text to find";
+  it("returns pass is true when text is found in the given DOM element", () => {
+    const domElement = { textContent: textToFind };
+
+    const result = toContainText(domElement, textToFind);
+
+    expect(result.pass).toBe(true);
+  });
+
+  it("returns pass is false when text is not found in the given DOM element", () => {
+    const domElement = { textContent: "" };
+
+    const result = toContainText(domElement, textToFind);
+
+    expect(result.pass).toBe(false);
+  });
+
+  it("returns a message that contains the source line if nomatch", () => {
+    const domElement = { textContent: "" };
+
+    const result = toContainText(domElement, textToFind);
+
+    expect(stripTerminalColor(result.message())).toContain(
+      `expect(element).toContainText("${textToFind}")`
+    );
+  });
+
+  it("returns a message that contains the source line if negated match", () => {
+    const domElement = { textContent: textToFind };
+
+    const result = toContainText(domElement, textToFind);
+
+    expect(stripTerminalColor(result.message())).toContain(
+      `expect(element).not.toContainText("${textToFind}")`
+    );
+  });
+
+  it("returns a message that contains the actual text", () => {
+    const domElement = { textContent: textToFind };
+    const result = toContainText(domElement, textToFind);
+
+    expect(stripTerminalColor(result.message())).toContain(
+      `Actual text: "${textToFind}"`
+    );
+  });
+});
