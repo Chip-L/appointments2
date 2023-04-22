c301c35a22eda73e6dba9ec2d4a2840def9c318d
clong desktop <chip_long@yahoo.com>
2023-04-21 19:05:05 -0600

    end of chapter 1

---
+++ src/Appointment.js
@@ -0,0 +1,1 @@
+export const Appointment = ({ customer }) => customer.firstName;
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { act } from "react-dom/test-utils";
+import { Appointment } from "../src/Appointment";
+
+let container;
 
 describe("Appointment", () => {
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
+  const render = (component) =>
+    act(() => ReactDOM.createRoot(container).render(component));
+
   it("renders the customer first name", () => {
     const customer = { firstName: "Ashley" };
-    const component = <Appointment customer={customer} />;
 
-    const container = document.createElement("div");
-    document.body.appendChild(container);
-
-    ReactDOM.createRoot(container).render(component);
+    render(<Appointment customer={customer} />);
 
     expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it("renders another customer first name", () => {
+    const customer = { firstName: "Jordan" };
+
+    render(<Appointment customer={customer} />);
+
+    expect(document.body.textContent).toContain("Jordan");
   });
 });
