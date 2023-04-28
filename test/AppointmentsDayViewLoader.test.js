473bd79728e09f807916f0b5eb3812d650191b6e
clong desktop <chip_long@yahoo.com>
2023-04-27 21:49:46 -0600

    added fetch appointment functionality

--- test/AppointmentsDayViewLoader.test.js
+++ test/AppointmentsDayViewLoader.test.js
@@ -6,14 +6,21 @@ import {
 } from "./reactTestExtensions";
 import { AppointmentsDayView } from "../src/AppointmentsDayView";
 import { AppointmentsDayViewLoader } from "../src/AppointmentsDayViewLoader";
+import { today, todayAt } from "./builders/time";
+import { fetchResponseOk } from "./builders/fetch";
 
 jest.mock("../src/AppointmentsDayView", () => ({
   AppointmentsDayView: jest.fn(() => <div id="AppointmentsDayView" />),
 }));
 
 describe("AppointmentsDayViewLoader", () => {
+  const appointments = [{ startsAt: todayAt(9) }, { startsAt: todayAt(10) }];
+
   beforeEach(() => {
     initializeReactContainer();
+    jest
+      .spyOn(global, "fetch")
+      .mockResolvedValue(fetchResponseOk(appointments));
   });
 
   it("renders an AppointmentsDayView", async () => {
@@ -26,6 +33,28 @@ describe("AppointmentsDayViewLoader", () => {
 
     expect(AppointmentsDayView).toBeCalledWith(
       { appointments: [] },
+      expect.anything()
+    );
+  });
+
+  it("fetches data when component is mounted", async () => {
+    const from = todayAt(0);
+    const to = todayAt(23, 59, 59, 999);
+
+    await renderAndWait(<AppointmentsDayViewLoader today={today} />);
+
+    expect(global.fetch).toBeCalledWith(`/appointments/${from}-${to}`, {
+      method: "GET",
+      credentials: "same-origin",
+      headers: { "Content-Type": "application/json" },
+    });
+  });
+
+  it("passes fetched appointments to AppointmentsDayView once they have loaded", async () => {
+    await renderAndWait(<AppointmentsDayViewLoader />);
+
+    expect(AppointmentsDayView).toHaveBeenLastCalledWith(
+      { appointments },
       expect.anything()
     );
   });
